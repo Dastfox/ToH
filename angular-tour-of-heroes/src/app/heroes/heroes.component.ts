1ab6ef26c07882c45419a408111bb65798e8aeda
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-25 17:07:58 +0200

    Splitting the details of heroes to feature component

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,10 +13,7 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
-  hero : Hero = {
-    id : 1,
-    name: 'Windstorm'
-  };
+
 
   constructor() { }
 
