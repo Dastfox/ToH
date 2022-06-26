b763e438f5b0f53fcd6c45cc6858e7a3121f0668
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-26 13:30:30 +0200

    adding data service (in memory)

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component' ;
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
  
 @NgModule({
   declarations: [
@@ -18,9 +21,14 @@ import { DashboardComponent } from './dashboard/dashboard.component' ;
     DashboardComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+      HttpClientInMemoryWebApiModule.forRoot(
+       InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,15 +3,14 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
   getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!;
@@ -19,5 +18,14 @@ export class HeroService {
     return of(hero); 
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+    ) { }
+
+  
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesUrl = 'api/heroes'; 
 }
